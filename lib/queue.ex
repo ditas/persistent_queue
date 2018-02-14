0b02cfae35cb11f3d0760538486925e1959a950d
ditas <ditasandditas@gmail.com>
2018-02-14 13:05:40 +0300

    Revertion done

--- lib/queue.ex
+++ lib/queue.ex
@@ -14,21 +14,21 @@ defmodule PerQ.Queue do
         {:ok, %{:stack => [], :operation_num => 0, :await_acks => []}}
     end
 
+    #################### Call ####################
+    def handle_call(:stack, state) do
+        stack = Map.get(state, :stack)
+        {:reply, {:ok, stack}, state}
+    end
     def handle_call({:revert, time, num}, _from, state) do
-
-#        ms1 = [{{:'$1',:'_'}, [{:>,:'$1',1}], [:true]}]
-#        ms2 = [{{:'$1',:'$2'}, [{:>,:'$1',1}], [:'$2']}]
-        ms = [{{:'_',:'$1',:'$2',:'$3'}, [{:>=,:'$1',time}], [:'$3']}]
-
+        ms = [{{:'_',:'$1',:'$2',:'$3'}, [{:>,:'$1',time}, {:>,:'$2',num}], [:'$3']}]
         selection = :ets.select(@tab, ms)
 
         IO.inspect(selection)
 
-        test = :ets.info(@tab)
+        current_stack = Map.get(state, :stack)
+        reverted_stack = handle_revertion(selection, current_stack)
 
-        IO.inspect(test)
-
-        {:reply, :ok, state}
+        {:reply, {:ok, reverted_stack}, state}
     end
     def handle_call({:add, value} = operation, _from, state) do
         current_stack = Map.get(state, :stack)
@@ -65,6 +65,7 @@ defmodule PerQ.Queue do
         {:reply, :ok, state}
     end
 
+    #################### Cast ####################
     def handle_cast(:state, state) do
 
         IO.inspect(state)
@@ -75,6 +76,7 @@ defmodule PerQ.Queue do
         {:noreply, state}
     end
 
+    #################### Info ####################
     def handle_info({:rejection_timeout, ref}, state) do
 
         IO.puts("timeout")
@@ -82,8 +84,15 @@ defmodule PerQ.Queue do
 
         current_stack = Map.get(state, :stack)
         current_awaits = Map.get(state, :await_acks)
+        current_operation_num = Map.get(state, :operation_num)
         state1 = case List.keytake(current_awaits, ref, 0) do
             {{_ref, _tref, value, _timestamp, _new_operation_num}, new_awaits} ->
+
+                reject_timestamp = :erlang.system_time(:millisecond)
+                reject_operation_num = current_operation_num + 1
+                reject_change = q_change(timestamp: reject_timestamp, operation_num: reject_operation_num, operation: {:reject, value})
+                :ets.insert(@tab, reject_change)
+
                 Map.put(state, :stack, [value|current_stack])
                 |> Map.put(:await_acks, new_awaits)
             _ ->
@@ -98,7 +107,7 @@ defmodule PerQ.Queue do
 
         current_awaits = Map.get(state, :await_acks)
         state1 = case List.keytake(current_awaits, ref, 0) do
-            {{_ref, tref, _value, timestamp, new_operation_num}, new_awaits} ->
+            {{_ref, tref, value, timestamp, new_operation_num}, new_awaits} ->
 
                 IO.puts("timer to cancel")
                 IO.inspect(tref)
@@ -107,7 +116,7 @@ defmodule PerQ.Queue do
 
                 IO.inspect(test)
 
-                new_change = q_change(timestamp: timestamp, operation_num: new_operation_num, operation: {:get, ref})
+                new_change = q_change(timestamp: timestamp, operation_num: new_operation_num, operation: {:get, value})
                 :ets.insert(@tab, new_change)
 
                 Map.put(state, :await_acks, new_awaits)
@@ -126,7 +135,7 @@ defmodule PerQ.Queue do
         {:noreply, state}
     end
 
-    # External functions
+    #################### External functions ####################
     def add(value) do
         GenServer.call(__MODULE__, {:add, value})
     end
@@ -135,7 +144,11 @@ defmodule PerQ.Queue do
         GenServer.call(__MODULE__, {:get, ref})
     end
 
-    def show_state do
+    def stack() do
+        GenServer.call(__MODULE__, :stack)
+    end
+
+    def show_state() do
         GenServer.cast(__MODULE__, :state)
     end
 
@@ -147,6 +160,26 @@ defmodule PerQ.Queue do
         GenServer.call(__MODULE__, {:revert, timestamp, num})
     end
 
-    # Internal function
+    #################### Internal function ####################
+    def handle_revertion(operations, stack) do
+        test = List.foldr(operations, stack, fn(operation, s) ->
+            invert(operation, s)
+        end)
 
+        IO.inspect(test)
+
+        test
+    end
+
+    def invert({:reject, value}, stack) do
+        [value|t] = stack
+        t ++ [value]
+    end
+    def invert({:get, value}, stack) do
+        stack ++ [value]
+    end
+    def invert({:add, _value}, stack) do
+        [_|t] = stack
+        t
+    end
 end
